1d9c10de343368a3c844c30f20fa908845669b0d
Brian <bvw20002010@gmail.com>
2025-01-31 18:05:40 -0500

    Update test.py

--- databruce/test.py
+++ databruce/test.py
@@ -12,19 +12,3 @@ from tools.scraping import scraper
 
 load_dotenv()
 from bs4 import BeautifulSoup as bs4
-
-df = pd.read_csv(
-    Path(r"C:\Users\bvw20\Documents\Personal\Databruce Project\sign requests.csv"),
-)
-
-with load_db() as conn:
-    cur = conn.cursor()
-
-    fix = []
-    print(df)
-
-    for row in df.itertuples():
-        cur.execute(
-            """UPDATE setlists SET sign_request = true WHERE event_id = %s and song_id = %s AND set_name <> ALL(ARRAY['Soundcheck', 'Rehearsal'])""",
-            (row.event, row.song),
-        )
